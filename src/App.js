d4b26c54d8a209842d3734ad403f69fdbfdafe3a
osezei <john.osezei@yahoo.com>
2023-03-11 00:17:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./component/Navbar";
+import CartContainer from "./component/CartContainer";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
