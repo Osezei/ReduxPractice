aa6881bff0ba278badf61e0ca4e6848f0a69e73c
osezei <john.osezei@yahoo.com>
2023-03-12 15:17:34 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import Navbar from "./component/Navbar";
 import CartContainer from "./component/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
